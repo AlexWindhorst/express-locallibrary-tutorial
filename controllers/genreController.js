6fecf3ded5308049b4d7b0c9439aee097b62048d
Alex Windhorst <windhorstalex@gmail.com>
2019-10-24 10:24:30 -0500

    Fixed genre Delete

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -134,14 +134,14 @@ exports.genre_delete_post = function(req, res, next) {
     }, function(err, results) {
         if (err) { return next(err); }
         // Success
-        if (results.genre_books.length > 0) {
+        if (results.genres_books.length > 0) {
             // genre has books. Render in same way as for GET route.
             res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
             return;
         }
         else {
             // genre has no books. Delete object and redirect to the list of genres.
-            Genre.findByIdAndRemove(req.body.id, function deleteGenre(err) {
+            Genre.findByIdAndRemove(req.params.id, function deleteGenre(err) {
                 if (err) { return next(err); }
                 // Success - go to genre list
                 res.redirect('/catalog/genres')
